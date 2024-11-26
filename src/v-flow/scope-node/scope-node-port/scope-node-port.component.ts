11832fb4d37ecc0f4a80c5fb17da798dce430d94
Vinsky Strauss TanHanSen <vinsky.tanhansen@etalytics.com>
2024-11-26 10:59:30 +0100

    separate custom node

---
+++ src/v-flow/scope-node/scope-node-port/scope-node-port.component.ts
@@ -0,0 +1,39 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  signal,
+} from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { VflowModule } from "ngx-vflow";
+import { fromEvent, take } from "rxjs";
+import { Port } from "../../../model/port.model";
+
+@Component({
+  selector: "eta-scope-node-ports",
+  templateUrl: "./scope-node-port.component.html",
+  styleUrls: ["./scope-node-port.component.scss"],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [VflowModule, MatTooltipModule],
+})
+export class ScopeNodeVPortComponent {
+  nodePorts = input<Port[]>();
+  editMode = input<boolean>();
+  position = input<"top" | "left" | "right">("left");
+
+  destroyRef = inject(DestroyRef);
+
+  handleClicked = signal(false);
+
+  onHandleMouseDown(): void {
+    this.handleClicked.set(true);
+    // When handle clicked, listen body mouseup to send handle click
+    fromEvent(window.document.body, "mouseup")
+      .pipe(take(1), takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.handleClicked.set(false));
+  }
+}
--- src/v-flow/scope-node/scope-node.component.ts
+++ src/v-flow/scope-node/scope-node.component.ts
@@ -4,11 +4,13 @@ import {
   computed,
   Input,
   input,
+  Signal,
 } from "@angular/core";
 import { CustomNodeComponent } from "ngx-vflow";
 import { CustomSvgComponent } from "../../background-svg/custom-svg/custom-svg.component";
 import { ElementData } from "../../model/element-data.model";
 import { FeatureModel } from "../../model/feature.model";
+import { Port } from "../../model/port.model";
 
 interface ScopeNodeData {
   feature: FeatureModel;
@@ -21,14 +23,36 @@ interface ScopeNodeData {
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div class="container">
-      <!-- <svg>
-        <svg:g custom-element [element]="element()"></svg:g>
-      </svg> -->
-      <span>{{ data()?.feature?.name }}</span>
+      <div class="node">
+        @if(data()?.feature?.elements) {
+
+        <svg:g custom-element [element]="data()?.feature?.elements!"></svg:g>
+
+        }
+        <span>{{ data()?.feature?.name }}</span>
+      </div>
     </div>
   `,
   styleUrls: ["./scope-node.component.scss"],
 })
 export class ScopeNodeComponent extends CustomNodeComponent<ScopeNodeData> {
   //Retrieve the data, so we can use it in html
+  name = computed(() => this.data()?.feature?.name);
+
+  //Get tge port
+  ports: Signal<Port[] | undefined> = computed(
+    () => this.data()?.feature?.ports
+  );
+
+  leftPorts: Signal<Port[] | undefined> = computed(() =>
+    this.ports()?.filter((port) => port.location === "left")
+  );
+
+  rightPorts: Signal<Port[] | undefined> = computed(() =>
+    this.ports()?.filter((port) => port.location === "right")
+  );
+
+  topPorts: Signal<Port[] | undefined> = computed(() =>
+    this.ports()?.filter((port) => port.location === "top")
+  );
 }
