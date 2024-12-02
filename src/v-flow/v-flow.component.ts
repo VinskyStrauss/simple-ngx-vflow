d85fe9c11423d70f8009699b14db2f916f0b3308
Vinsky Strauss TanHanSen <vinsky.tanhansen@etalytics.com>
2024-12-02 10:43:39 +0100

    add coordinate calculation based on GeoJSON

--- src/v-flow/v-flow.component.ts
+++ src/v-flow/v-flow.component.ts
@@ -16,6 +16,7 @@ import {
   VflowComponent,
   Background,
   ConnectionSettings,
+  NodeChange,
 } from "ngx-vflow";
 import { BackgroundSvgComponent } from "../background-svg/background-svg.component";
 import { GrafenHauserComponent } from "../background-svg/grafenhauser-svg.component";
@@ -63,10 +64,12 @@ export class SimpleVflowComponent {
 
   //Create Node
   public createNode(feature: FeatureModel): Node {
+    //Calculate the coordinate of the feature
+    const { xPixel, yPixel } = this.calculateCoordinate(feature);
     return {
       id: crypto.randomUUID(),
       data: { feature },
-      point: { x: 0, y: 0 },
+      point: { x: xPixel, y: yPixel },
       type: ScopeNodeComponent,
     };
   }
@@ -109,5 +112,84 @@ export class SimpleVflowComponent {
         },
       },
     ];
+  }
+
+  //Console.log the position change
+  public onPositionChange(changes: NodeChange[]) {
+    console.log("Position change", changes);
+  }
+
+  //Calculate the coordinate of the feature in ngx-vflow
+  private calculateCoordinate(feature: FeatureModel) {
+    const height = 1069;
+    const width = 1220;
+    // Map extent
+    const mapExtent = {
+      left: 8.634022529410913, // Longitude for top-left corner
+      bottom: 49.8835094952137, // Latitude for bottom-right corner
+      right: 8.638534952430376, // Longitude for bottom-right corner
+      top: 49.88613737107883, // Latitude for top-left corner
+    };
+    const [centroidX, centroidY] = this.calculatePolygonCentroid(feature);
+    // Calculations
+    const xPixel =
+      ((centroidX - mapExtent.left) / (mapExtent.right - mapExtent.left)) *
+      width;
+    const yPixel =
+      height -
+      ((centroidY - mapExtent.bottom) / (mapExtent.top - mapExtent.bottom)) *
+        height;
+    console.log("Calculated coordinates:", { xPixel, yPixel });
+    return { xPixel, yPixel };
+  }
+
+  //Calculate the centroid of the polygon
+  private calculatePolygonCentroid(feature: FeatureModel): [number, number] {
+    let xSum = 0;
+    let ySum = 0;
+    let area = 0;
+
+    if (!feature.elements.myGeoJson) {
+      return [0, 0]; // Return default if no GeoJSON
+    }
+
+    // Check if the GeoJSON type is "Feature"
+    if (feature.elements.myGeoJson.type === "Feature") {
+      const geometry = feature.elements.myGeoJson.geometry;
+
+      // Ensure the geometry type is "Polygon"
+      if (geometry.type === "Polygon") {
+        const coordinates = geometry.coordinates[0]; // First set of coordinates for the polygon
+        const vertexCount = coordinates.length;
+
+        // Ensure the polygon is closed
+        if (
+          coordinates[0][0] !== coordinates[vertexCount - 1][0] ||
+          coordinates[0][1] !== coordinates[vertexCount - 1][1]
+        ) {
+          coordinates.push(coordinates[0]); // Close the polygon if necessary
+        }
+
+        for (let i = 0; i < coordinates.length - 1; i++) {
+          const [x1, y1] = coordinates[i];
+          const [x2, y2] = coordinates[i + 1];
+
+          const partialArea = x1 * y2 - x2 * y1;
+
+          xSum += (x1 + x2) * partialArea;
+          ySum += (y1 + y2) * partialArea;
+          area += partialArea;
+        }
+
+        area *= 0.5;
+
+        const centroidX = xSum / (6 * area);
+        const centroidY = ySum / (6 * area);
+
+        return [centroidX, centroidY];
+      }
+    }
+
+    return [0, 0]; // Default return in case of invalid input
   }
 }
