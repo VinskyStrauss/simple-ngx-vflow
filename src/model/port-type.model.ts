484792e509f312e2951e61db9ba9436cd3033650
stvitanh <vinsky.s.tanhansen@stud.h-da.de>
2024-12-11 19:10:54 +0100

    change color

--- src/model/port-type.model.ts
+++ src/model/port-type.model.ts
@@ -3,5 +3,5 @@ export type PortType = "Cooling" | "Heating" | "Electricity";
 export const PortTypeColors: Record<PortType, string> = {
   Cooling: "blue",
   Heating: "red",
-  Electricity: "yellow",
+  Electricity: "green",
 };
